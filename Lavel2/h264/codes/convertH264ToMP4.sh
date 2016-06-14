49b0b2ca435183cd0636c19ca481bad6b8018b1d
ankigupt0000 <ankigupt@gmail.com>
2016-06-14 21:04:36 +0530

    Update convertH264ToMP4.sh

--- Lavel2/h264/codes/convertH264ToMP4.sh
+++ Lavel2/h264/codes/convertH264ToMP4.sh
@@ -1,4 +1,4 @@
-ffmpeg -i file1.h264 -c:v libx264 -c:a copy file1.mp4
-ffmpeg -i file2.h264 -c:v libx264 -c:a copy file2.mp4
-ffmpeg -i file3.h264 -c:v libx264 -c:a copy file3.mp4
-ffmpeg -i file4.h264 -c:v libx264 -c:a copy file4.mp4
+ffmpeg -i ../output/file1.h264 -c:v libx264 -c:a copy ../output/file1.mp4
+ffmpeg -i ../output/file2.h264 -c:v libx264 -c:a copy ../output/file2.mp4
+ffmpeg -i ../output/file3.h264 -c:v libx264 -c:a copy ../output/file3.mp4
+ffmpeg -i ../output/file4.h264 -c:v libx264 -c:a copy ../output/file4.mp4
