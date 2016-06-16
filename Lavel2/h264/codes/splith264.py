edd8fc6e9b595349abbbda2de77a123a824c3556
ankigupt0000 <ankigupt@gmail.com>
2016-06-16 11:35:02 +0530

    Update splith264.py

--- Lavel2/h264/codes/splith264.py
+++ Lavel2/h264/codes/splith264.py
@@ -16,70 +16,56 @@ try:
         byte=f.read()
         lst=byte.split(b'\x00\x00\x01')
         print(len(lst))
+        b=0
+        file=0
         for b in range(len(lst)):
-            #print(bytes((lst[b])[0:1]))
+                if (lst[b])[:1]==b'\x65' and lst[b-1][5:6] == b'\x00':
+                        meta=int.from_bytes((lst[b-1])[3:4],byteorder='big')
+                        pmeta=int.from_bytes(lst[b-4][6:7],byteorder='big')
+                        pseqmeta=int.from_bytes(lst[b-4][0:1],byteorder='big')
+                        if count<4:
+                                    last4[count]=meta
+                                    target[count].write(b'\x00\x00\x01'+lst[b-4]+b'\x00\x00\x01'+lst[b-3]+b'\x00\x00\x01'+lst[b-2]+b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
+                                    plast4[count]=pmeta
+                                    pseq[count]=pseqmeta
+                                    count+=1
+                        else:
+                                break;
                 
+        while b < len(lst):
             # I Frame  distinguished and saved in different files.
-            if (lst[b])[:1]==b'\x65':
+            if (lst[b])[:1]==b'\x65' and lst[b-1][5:6] == b'\x00':
                     meta=int.from_bytes((lst[b-1])[3:4],byteorder='big')
                     pmeta=int.from_bytes(lst[b-4][6:7],byteorder='big')
                     pseqmeta=int.from_bytes(lst[b-4][0:1],byteorder='big')
-                    #print()
-                    # print('I', lst[b-4])
-                    # print()
-                    #print(meta)
-                    if count<4:
-                            last4[count]=meta
-                            target[count].write(b'\x00\x00\x01'+lst[b-4]+b'\x00\x00\x01'+lst[b-3]+b'\x00\x00\x01'+lst[b-2]+b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                            plast4[count]=pmeta
-                            pseq[count]=pseqmeta
-                    elif  meta == (last4[0]+2)%256:
-                            #print(meta, last4[0])
-                            target[0].write(b'\x00\x00\x01'+lst[b-4]+b'\x00\x00\x01'+lst[b-3]+b'\x00\x00\x01'+lst[b-2]+b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                            last4[0]=meta
-                            plast4[0]=pmeta
-                            pseq[0]=pseqmeta
-                    elif  meta == (last4[1]+2)%256:
-                            #print(meta, last4[1])
-                            target[1].write(b'\x00\x00\x01'+lst[b-4]+b'\x00\x00\x01'+lst[b-3]+b'\x00\x00\x01'+lst[b-2]+b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                            last4[1]=meta
-                            plast4[1]=pmeta
-                            pseq[1]=pseqmeta
-                    elif  meta == (last4[2]+2)%256:
-                            #print(meta, last4[2])
-                            target[2].write(b'\x00\x00\x01'+lst[b-4]+b'\x00\x00\x01'+lst[b-3]+b'\x00\x00\x01'+lst[b-2]+b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                            last4[2]=meta
-                            plast4[2]=pmeta
-                            pseq[2]=pseqmeta
-                    elif  meta == (last4[3]+2)%256:
-                            #print(meta, last4[3])
-                            target[3].write(b'\x00\x00\x01'+lst[b-4]+b'\x00\x00\x01'+lst[b-3]+b'\x00\x00\x01'+lst[b-2]+b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                            last4[3]=meta
-                            plast4[3]=pmeta
-                            pseq[3]=pseqmeta
-                    count+=1
-            #print(len(last4))
-            #print(len(plast4))
-            if (lst[b])[:1] ==b'\x61' and lst[b-1][5:6] == b'\x01' :
-                   pmeta=int.from_bytes(lst[b-1][6:7],byteorder='big')
-                   pseqmeta=int.from_bytes(lst[b-1][0:1],byteorder='big')
-                   if pmeta == (plast4[0]+40)%256 and pseqmeta == pseq[0]+1:
-                           target[0].write(b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                           plast4[0]=pmeta
-                           pseq[0]=pseqmeta
-                   if pmeta == (plast4[1]+40)%256 and pseqmeta == pseq[1]+1:
-                           target[1].write(b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                           plast4[1]=pmeta
-                           pseq[1]=pseqmeta
-                   if pmeta  == (plast4[2]+40)%256 and pseqmeta == pseq[2]+1:
-                           target[2].write(b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                           plast4[2]=pmeta
-                           pseq[2]=pseqmeta
-                   if pmeta == (plast4[3]+40)%256 and pseqmeta == pseq[3]+1:
-                           target[3].write(b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
-                           plast4[3]=pmeta
-                           pseq[3]=pseqmeta
-                   pcount+=1
+                    for j in range(4):
+                            if  meta == (last4[j]+2)%256:
+                                    #print(meta, last4[0])
+                                    target[j].write(b'\x00\x00\x01'+lst[b-4]+b'\x00\x00\x01'+lst[b-3]+b'\x00\x00\x01'+lst[b-2]+b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
+                                    last4[j]=meta
+                                    plast4[j]=pmeta
+                                    pseq[j]=pseqmeta
+                                    file=j
+                    
+                    nextImeta=0
+                    nextI=len(lst)
+                    for j in range(b+1,len(lst)):
+                            nextImeta=int.from_bytes((lst[j-1])[3:4],byteorder='big')
+                            if nextImeta == (meta+2)%256:
+                                    nextI=j
+                                    break;
+
+                    for j in range(b+1,nextI):
+                        if (lst[j])[:1] ==b'\x61' and lst[j-1][5:6] == b'\x01' :
+                           #pmeta=int.from_bytes(lst[j-1][6:7],byteorder='big')
+                           pseqmeta=int.from_bytes(lst[j-1][0:1],byteorder='big')
+                           for k in range(4):        
+                                   if  pseqmeta == pseq[k]+1: 
+                                           target[file].write(b'\x00\x00\x01'+lst[b-1]+b'\x00\x00\x01'+lst[b])
+                                           plast4[k]=pmeta
+                                           pseq[k]=pseqmeta
+                                   pcount+=1
+            b+=1
 finally:
     f.close()
     for i in range(d):
