8eb139da2dd022c463851a4c0f66554b8690baa6
ankigupt0000 <ankigupt@gmail.com>
2016-06-16 18:56:02 +0530

    Delete convertH264ToMP4.sh

--- Lavel2/h264/codes/convertH264ToMP4.sh
+++
@@ -1,4 +0,0 @@
-ffmpeg -i ../output/file1.h264 -c:v libx264 -c:a copy ../output/file1.mp4
-ffmpeg -i ../output/file2.h264 -c:v libx264 -c:a copy ../output/file2.mp4
-ffmpeg -i ../output/file3.h264 -c:v libx264 -c:a copy ../output/file3.mp4
-ffmpeg -i ../output/file4.h264 -c:v libx264 -c:a copy ../output/file4.mp4
