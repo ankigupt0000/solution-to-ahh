a6499070ef0826ed2c46b84275eab663d8176bfd
ankigupt0000 <ankigupt@gmail.com>
2016-06-18 19:17:40 +0530

    Create 263mp4TO264mp4.bat

---
+++ Lavel2/arc/code/263mp4TO264mp4.bat
@@ -0,0 +1,2 @@
+ffmpeg -i "../output/2 - 73602  - ID Camera 2 - 01_25 - 01_42.mp4" -f mp4 -c:v libx264 "../output/2- 73602  - ID Camera 2 - 01_25 - 01_42_264.mp4"264.mp4"
+ffmpeg -i "../output/1 - 73602  - ID Camera 1 - 01_25 - 01_42.mp4" -f mp4 -c:v libx264 "../output/1 - 73602  - ID Camera 1 - 01_25 - 01_42_264.mp4"
